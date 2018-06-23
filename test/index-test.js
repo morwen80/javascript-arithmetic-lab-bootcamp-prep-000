68401af9ddf07d2e5316f31650cf810af176d83b
Valeria Ragonese <ribesnero80@gmail.com>
2018-06-23 17:02:20 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -9,6 +9,12 @@ function subtract(a, b) {
   return a - b
 }
 
+function multiply(a, b) {
+  return a * b
+}
+
+
+
 beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
   b = Math.floor(Math.random() * 1000)
